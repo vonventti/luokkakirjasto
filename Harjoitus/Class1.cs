7e0f78a5a0f3a67dcaab34b496fc7e88bcf8b913
anders von wendt <andersvonwendt@gmail.com>
2022-06-27 14:52:57 +0300

    metodi 2

--- Harjoitus/Class1.cs
+++ Harjoitus/Class1.cs
@@ -4,24 +4,44 @@ namespace Harjoitus
 {
     public class Class1
     {
-     public static bool DateTimeCheck(DateTime a)
+        public static bool DateTimeCheck(DateTime a)
         {
             bool timeCheck;
             if (DateTime.Now > a)
             {
                 return timeCheck = false;
-                
+
             }
             else if (DateTime.Now < a)
             {
                 return timeCheck = true;
-                
+
             }
             else
             {
                 return timeCheck = true;
             }
         }
-        public static bool OverDueOrNot(DateTime a)
+        public static bool OverDueOrNot(DateTime aloitusPvm, double aikaArvio)
+        {
+            bool timeCheck;
+            TimeSpan arvioituOpiskeluAika = TimeSpan.FromDays(aikaArvio);
+            if (DateTime.Now > (aloitusPvm + arvioituOpiskeluAika))
+            {
+                return timeCheck = true;
+            }
+            else if (DateTime.Now < (aloitusPvm + arvioituOpiskeluAika))
+            {
+                return timeCheck = false;
+            }
+            else
+            {
+                return timeCheck = true;
+            }
+        }
+
+
+
+        // Metodi, joka kertoo opiskelun aloitusajan ja arvioidun opiskeluajan perusteella onko opiskelu myöhässä vai ei.
     }
 }
